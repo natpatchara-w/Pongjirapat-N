4c8df53142f24ff70e4eaf593b5d6d6569a371c5
natpatchara.pon <n.pongjirapat@gmail.com>
2024-11-06 20:07:18 +0700

    Updata App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Test github actions</p>
     </>
   );
 }
